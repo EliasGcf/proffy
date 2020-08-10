deba93192a93a52a6d6d2a16113120f680874107
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-08-10 20:41:38 -0300

    chore: Add GlobalStyle and ThemeProvider

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import ligthTheme from './styles/themes/light';
+import GlobalStyles from './styles/global';
 
 import './assets/styles/global.css';
 import Routes from './routes';
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={ligthTheme}>
+      <Routes />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ packages/web/src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    font-size: 60%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100vh;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.background};
+  }
+
+  #root {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  body,
+  input,
+  button,
+  textarea {
+    font: 500 1.6rem Poppins;
+    color: ${({ theme }) => theme.colors.textBase};
+  }
+
+  .container {
+    width: 90vw;
+    max-width: 700px;
+  }
+
+  @media (min-width: 700px) {
+    :root {
+      font-size: 62.5%;
+    }
+  }
+`;
