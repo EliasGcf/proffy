4834ac9e6fb9ca428ca5aab5775f1970810bbba9
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-08-13 13:27:52 -0300

    chore(web): Integrate Checkbox page component with Unform

--- packages/web/src/pages/SignIn/components/CheckBox/index.tsx
+++ packages/web/src/pages/SignIn/components/CheckBox/index.tsx
@@ -1,34 +1,62 @@
-import React, { InputHTMLAttributes, useState, useCallback } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useState,
+  useCallback,
+  useRef,
+  useEffect,
+} from 'react';
+import { useField } from '@unform/core';
+
+import { CheckIcon } from '../../../../assets/images/icons';
 
 import { CheckBoxIcon, LabelContainer } from './styles';
-import { CheckIcon } from '../../../../assets/images/icons';
 
 interface CheckBoxProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
   name: string;
-  label: string;
 }
 
-const CheckBox: React.FC<CheckBoxProps> = ({ name, label, ...rest }) => {
+const CheckBox: React.FC<CheckBoxProps> = ({ name, ...rest }) => {
   const [isChecked, setIsChecked] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { fieldName, registerField, defaultValue = false } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      getValue: (ref: HTMLInputElement) => {
+        return ref.checked || false;
+      },
+      setValue: (ref: HTMLInputElement, value: boolean) => {
+        ref.checked = value;
+        setIsChecked(value);
+      },
+      clearValue: (ref: HTMLInputElement) => {
+        ref.checked = false;
+        setIsChecked(false);
+      },
+    });
+  }, [defaultValue, fieldName, registerField]);
 
   const handleToggleChecked = useCallback(() => {
     setIsChecked(state => !state);
   }, []);
 
   return (
-    <LabelContainer htmlFor={`${name}`}>
+    <LabelContainer htmlFor={name}>
       <CheckBoxIcon isChecked={isChecked}>
         <CheckIcon />
       </CheckBoxIcon>
       <input
+        ref={inputRef}
         onChange={handleToggleChecked}
+        defaultChecked={defaultValue}
         type="checkbox"
-        id={`${name}`}
-        name={`${name}`}
+        id={name}
         {...rest}
       />
-      {label}
+      Lembrar-me
     </LabelContainer>
   );
 };
