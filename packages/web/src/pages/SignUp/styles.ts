0944d3382f9815f76646a5023ca7f20cc55802b3
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-08-15 17:50:23 -0300

    chore(web): Remove unused import

--- packages/web/src/pages/SignUp/styles.ts
+++ packages/web/src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Form as Unform } from '@unform/web';
 
 import backgroundAsset from '../../assets/images/background-asset.svg';
