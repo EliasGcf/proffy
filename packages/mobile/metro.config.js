16d7391146957d3ee8f443fcfff4ec83968b6d3c
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-08-23 13:46:14 -0300

    chore(mobile): CRNA init

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,3 +1,5 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
-
-module.exports = createMetroConfiguration(__dirname);
+module.exports = {
+  transformer: {
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+  },
+};
